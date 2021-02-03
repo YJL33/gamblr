8046d1fd1ef3bc0beb62424f0bd38e1a2e3a9d96
YJL33 <yunjun.l33@gmail.com>
2021-02-04 02:07:14 +0800

    Add firebase initialization and LogIn/LogOut mechanism.

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:gamblr/firebase/user_repository.dart';
 
 class LoginPage extends StatelessWidget {
+  final UserRepository _userRepository;
+
+  LoginPage({UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository;
+
   @override
   Widget build(BuildContext context) {
+    String _usr = 'unknown';
+    _userRepository.getUser().then((String usr) {
+      _usr = usr;
+    });
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: FlatButton(
-          child: Text('Back'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+            child: Text('now signed in as...'),
+            onPressed: () => showDialog(
+              context: context,
+              builder: (_) => AlertDialog(
+                backgroundColor: Colors.blue,
+                title: Text('User:'),
+                content: Text('$_usr'),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            child: Text('Login with Google'),
+            onPressed: () => _userRepository.signInWithGoogle(),
+          ),
+          ElevatedButton(
+            child: Text('Sign out'),
+            onPressed: () => _userRepository.signOut(),
+          )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:gamblr/firebase/user_repository.dart';
 import 'package:gamblr/homePage.dart';
 import 'package:gamblr/loginPage.dart';
 
-void main() {
+void main() async {
+  // Initialize FlutterFire
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Gamblr',
-      routes: {
-        '': (context) => MyHomePage(),
-        '/login': (context) => LoginPage(),
+    final UserRepository _userRepository = UserRepository();
+
+    return FutureBuilder(
+      builder: (context, snapshot) {
+        // if (snapshot.hasError) {
+        //   return SomethingWentWrong(errlog: snapshot.error.toString());
+        // }
+        return MaterialApp(
+          title: 'Gamblr',
+          debugShowCheckedModeBanner: false,
+          routes: {
+            '': (context) => MyHomePage(),
+            '/login': (context) => LoginPage(userRepository: _userRepository),
+          },
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: MyHomePage(title: 'Gamblr Home Page'),
+        );
       },
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Gamblr Home Page'),
     );
   }
+
+  // SomethingWentWrong({String errlog}) {
+  //   return Column(
+  //     children: <Widget>[
+  //       Text('$errlog'),
+  //     ],
+  //   );
+  // }
 }
